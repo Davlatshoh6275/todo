d542f8ce006ccc105a94f79507e9cd58df974884
Davlatshoh <davlatshoh6275@gmail.com>
2023-09-05 13:29:14 +0300

    created js functions

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 const inputBox = document.querySelector("#inputBox")
 const listConteiner = document.querySelector("#listConteiner")
 
-function addTask(){
-    if(inputBox.value === '') {
-        alert("You must write something!")
-    } else {
-        let li = document.createElement("li")
-        li.innerHTML = inputBox.value;
-        listConteiner.appendChild(li)
-    }
-    console.log(inputBox.value);
+let tasks = []
+let task = ""
+
+inputBox.onchange = (e) => {
+    task = e.target.value
 }
+
+
+
+function addTask() {
+
+
+    // let obj = {
+    //     status: false,
+    //     task,
+    //     id: Math.random()
+    // }
+    // tasks.push(obj)
+
+    // task = ''
+    // inputBox.setAttribute("value", '')
+
+    // console.log(tasks);
+
+    // const arr = tasks.map((item, index) => (`<li>${item.task}</li>`))
+
+    // for (let i of arr) {
+    //     listConteiner.innerHTML += i
+    // }
+}
+
+
+
