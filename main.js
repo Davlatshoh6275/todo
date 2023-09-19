b67abc899ce98e18ea089eb5f79fe5e98ac8a275
Davlatshoh <davlatshoh6275@gmail.com>
2023-09-19 12:24:46 +0300

    modied js file

--- main.js
+++ main.js
@@ -1,35 +1,17 @@
 const inputBox = document.querySelector("#inputBox")
 const listConteiner = document.querySelector("#listConteiner")
 
-let tasks = []
-let task = ""
-
-inputBox.onchange = (e) => {
-    task = e.target.value
-}
-
-
 
 function addTask() {
 
+    if (inputBox.value === '') {
+        alert("you must write something!")
+    } else {
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value
+        listConteiner.appendChild(li)
+    }
 
-    // let obj = {
-    //     status: false,
-    //     task,
-    //     id: Math.random()
-    // }
-    // tasks.push(obj)
-
-    // task = ''
-    // inputBox.setAttribute("value", '')
-
-    // console.log(tasks);
-
-    // const arr = tasks.map((item, index) => (`<li>${item.task}</li>`))
-
-    // for (let i of arr) {
-    //     listConteiner.innerHTML += i
-    // }
 }
 
 
